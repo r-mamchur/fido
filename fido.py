daeb7080ffef6e1ab2ea0a7081dd08a52022a79b
Roman Mamchur <r_mamchur@ukr.net>
2019-01-09 01:02:11 +0200

    fix error when n=1,2

--- fido.py
+++ fido.py
@@ -1,7 +1,14 @@
 def fido(n):
+   if n == 1:
+      return [1]
+   if n == 1:
+      return [1,1]
+
    s_l = [1,1]
    for  k in range(2,n) :
        s_l.append( s_l[k-2] + s_l[k-1] )
    return s_l
+
+
 
 print(fido(6))
