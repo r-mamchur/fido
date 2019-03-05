4d537b0dd241ae0c384459f5c4580ce391afb5a1
Roman Mamchur <r_mamchur@ukr.net>
2019-03-06 00:30:02 +0200

    fido.py: fix variable as shell

--- fido.py
+++ fido.py
@@ -4,10 +4,10 @@ def fido(n):
    if n == 1:
       return [1]
 
-   s_l = [1,1]
+   fn = [1,1]
    for  k in range(2,n) :
-       s_l.append( s_l[k-2] + s_l[k-1] )
-   return s_l
+       fn.append( fn[k-2] + fn[k-1] )
+   return fn
 
 
 print(fido(1))
